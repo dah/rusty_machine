ecc57f8ad4828efa3f161a68767172b77ed6254e
Dan Hancu <dan@hancu.org>
2022-05-30 00:44:59 +0100

    added HC process

--- src/machine/recipe.rs
+++ src/machine/recipe.rs
@@ -149,4 +149,73 @@ impl Recipe {
         ];
         Self::new(steps)
     }
+
+    pub fn hc() -> Self {
+        let steps = vec![
+            Step {
+                tank_type: TankType::Etch,
+                process_time: 500,
+                actual_time: 0,
+                remaining_time: 500,
+                extra_time: 0,
+            },
+            Step {
+                tank_type: TankType::EtchRinse,
+                process_time: 180,
+                actual_time: 0,
+                remaining_time: 180,
+                extra_time: 0,
+            },
+            Step {
+                tank_type: TankType::Clean,
+                process_time: 240,
+                actual_time: 0,
+                remaining_time: 240,
+                extra_time: 0,
+            },
+            Step {
+                tank_type: TankType::CleanRinse,
+                process_time: 180,
+                actual_time: 0,
+                remaining_time: 180,
+                extra_time: 0,
+            },
+            Step {
+                tank_type: TankType::DIRinse,
+                process_time: 500,
+                actual_time: 0,
+                remaining_time: 500,
+                extra_time: 0,
+            },
+            Step {
+                tank_type: TankType::Oven,
+                process_time: 240,
+                actual_time: 0,
+                remaining_time: 240,
+                extra_time: 0,
+            },
+            Step {
+                tank_type: TankType::Cool,
+                process_time: 240,
+                actual_time: 0,
+                remaining_time: 240,
+                extra_time: 0,
+            },
+            Step {
+                tank_type: TankType::Lacquer,
+                process_time: 180,
+                actual_time: 0,
+                remaining_time: 180,
+                extra_time: 0,
+            },
+            Step {
+                tank_type: TankType::Oven,
+                process_time: 240,
+                actual_time: 0,
+                remaining_time: 240,
+                extra_time: 0,
+            },
+        ];
+        Self::new(steps)
+    }
 }
